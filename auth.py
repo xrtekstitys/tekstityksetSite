c859e9a0bc9936ce4f0e11e8cffff439b4e15a1b
botsarefuture <101255718+botsarefuture@users.noreply.github.com>
2022-09-01 13:02:58 +0300

    index, auth

--- auth.py
+++ auth.py
@@ -1,5 +1,6 @@
 class auth():
   def check_auth(request):
     if 'matrix' in request.cookies:
-        return render_template(f'{language}/index.html')
-      else:
+      return True
+    else:
+      return False
