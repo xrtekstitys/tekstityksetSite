a46c41c1ce590e85e7a0f4c7d9b3803331777b9f
botsarefuture <101255718+botsarefuture@users.noreply.github.com>
2022-10-19 10:32:12 +0300

    deleted things

--- sekavat.py
+++
@@ -1,9 +0,0 @@
-@id_route("/signup-1/")
-def signup_id1():
-	return render_template("signup-1.html")
-@id_route("/signup-1/", methods=["POST"])
-def signup_send1():
-	username = request.form.get("username")
-	password = request.form.get("password")
-	matrix.create_user(request)
-	return f"User {username} created with password {password}, chat.xrtekstitys.fi"
